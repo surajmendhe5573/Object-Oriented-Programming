ddd7f1c29da13b1c83d00454968201be568564fd
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-22 20:59:14 +0530

    changes

--- practice.cpp
+++ practice.cpp
@@ -1,36 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class Animal{
-    public:
-        virtual void makeSound(){
-            cout<<"Animals are making sound"<<endl;
-        }
-};
-
-class Dog: public Animal{
-    public:
-        void makeSound()override{
-            cout<<"Dog barks"<<endl;
-        }
-};
-
-class Cat: public Animal{
-    public:
-        void makeSound()override{
-            cout<<"Cat meows"<<endl;
-        }
-};
-
 int main(){
-    Animal* animal1= new Dog();
-    Animal* animal2= new Cat();
 
-    animal1->makeSound();
-    animal2->makeSound();
-
-    delete animal1;
-    delete animal2;
+    cout<<"Welcome In the Oops";
 
     return 0;
 }
