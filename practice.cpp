d3109ce2aebad9e3e8fd15850345298cc37ca441
surajmendhe5573 <surajmendhe470@gmail.com>
2025-05-11 08:45:08 +0530

    chages

--- practice.cpp
+++ practice.cpp
@@ -1,47 +1,69 @@
-// Hybrid Inheritance=> It is a combination of multiple and multilevel inheritance.
 #include<bits/stdc++.h>
 using namespace std;
 
-// base class
-class Vehicle{
-    public:
-        void fuelUp(){
-            cout<<"Vehicle is fueling"<<endl;
-        }
-};
+class Customer{
+    string name;
+    int account_number;
+    int balance;
+    int age;
 
-// intermediate derived class
-class Engine: public Vehicle{
-    public:
-        void startEngine(){
-            cout<<"Engine has started"<<endl;
-        }
-};
+    bool isValidAmount(int amount){
+        return amount > 0;
+    }
 
-// final derived class
-class Wheels: public Vehicle{
-    public:
-        void rotateWheels(){
-            cout<<"Wheels are rotating"<<endl;
-        }
-};
+    bool isValidAge(int age){
+        return age>0 && age<=100;
+    }
 
-class Car: public Engine, public Wheels{
     public:
-        void drive(){
-            cout<<"Car is driving"<<endl;
+        Customer(string name, int account_number, int balance, int age){
+            this-> name= name;
+            this->account_number= account_number;
+            this->balance= balance;
+            this->age= age;
+        };
+
+        void deposit(int amount){
+            if(isValidAmount(amount)){
+                balance += amount;
+                cout<<"Deposited: "<<amount<<endl;
+            }else{
+                cout<<"Invalid amount"<<endl;
+            }
+        };
+
+        void withdraw(int amount){
+            if(isValidAmount(amount) && amount <= balance){
+                balance -= amount;
+                cout<<"Withdrawn: "<<amount<<endl;
+            }else{
+                cout<<"Invalid or Insufficient balance"<<endl;
+            }
+        };
+
+        void updateAge(int age){
+            if(isValidAge(age)){
+               this->age= age;
+               cout<<"Age updated successfully !"<<endl;
+            }else{
+            cout<<"Invalid age !"<<endl;
+            }
+        };
+
+        void diaplay(){
+            cout<<"Account Holder: "<<name<<", Account Number: "<<account_number<<", Balance: "<<balance<<", Age: "<<age<<endl;
         }
+        
 };
 
 int main(){
-    Car myCar;
+    Customer A1("Suraj Mendhe", 101, 800, 21);
 
-    myCar.startEngine();
-    myCar.rotateWheels();
-    myCar.drive();
+    A1.deposit(300);
+    A1.withdraw(200);
+    A1.updateAge(22);
 
-    myCar.Engine::fuelUp();
-    myCar.Wheels::fuelUp();
+    A1.diaplay();
 
     return 0;
 }
